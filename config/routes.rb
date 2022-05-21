b6a45430ff8db880f0cad5fb0ce3599255592556
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-05-21 07:42:00 +0000

    [Start]3-2投稿機能を作ろう

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'lists/edit'
   get'/top'=>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ post 'lists'=>'lists#create'
+
 end
