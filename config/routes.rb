b66dccbd616cea2027e3e932f5e893f9218e8ef7
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-05-28 04:09:43 +0000

    [Add]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
   get 'lists/edit'
-  get'/top'=>'homes#top'
+  get 'top'=>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
  post 'lists'=>'lists#create'
-
+ get 'lists'=>'lists#index'
+ get 'lists/:id'=>'lists#show',as: 'list'
+# as:と'list'の間にスペースがないと適用されないので注意
 end
